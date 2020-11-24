aded4a438b2d02afec6610e9666f163a4810eb23
Edwing <Edwing@DESKTOP-VC8LGV5>
2020-11-24 22:48:14 +0100

    First commit

---
+++ src/javaGit/MainClassProject.java
@@ -0,0 +1,10 @@
+package javaGit;
+
+public class MainClassProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World !");
+	}
+
+}
